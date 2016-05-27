f7f133271b36551ffb668495b9aeafbad0ce9f93
FroopleXP <connor@frooplexp.com>
2016-05-27 20:19:04 +0100

    Tidy up

--- minder-client.js
+++ minder-client.js
@@ -32,12 +32,7 @@ var api = express.Router();
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors());
 app.use(cookieParser());
 app.use(expressSession({
 	secret: config.auth.secret,
