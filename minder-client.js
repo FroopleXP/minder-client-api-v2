9dc23635eb13538433bd1ac146a18337155f197f
FroopleXP <connor@frooplexp.com>
2016-05-27 18:22:29 +0100

    Removed CORS

--- minder-client.js
+++ minder-client.js
@@ -32,6 +32,12 @@ var api = express.Router();
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(cookieParser());
 app.use(expressSession({
 	secret: config.auth.secret,
@@ -42,7 +48,6 @@ app.use(expressSession({
     },
     rolling: true
 }));
-app.use(cors({ origin: "*" }));
 app.use("/views", express.static(__dirname + '/views'));
 app.use('/api', api);
 
